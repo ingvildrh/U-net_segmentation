28a1e443fb0909de532e80e518bc826692441402
Ingvild Rustad Haugen <ingvilrh@ntnu.no>
2023-05-02 17:04:35 +0200

    fixed calculations between pixels body and wound

--- calculate_ulcer_score.py
+++ calculate_ulcer_score.py
@@ -10,20 +10,27 @@ body_json = json.load(open('body_pixels.json'))
 
 ulcer_json = json.load(open('ulcer_pixels.json'))
 
+body_values = []
+ulcer_values = []
+image_ids = []
+
 ulcer_factor_dict = {}
 
-
 for b_key, b_pxls in body_json.items():
-        b_key = b_key.split('_')[1]
-        for u_key, u_pxls in ulcer_json.items():
-                u_key = u_key.split('_')[1]
-                if str(u_key) in str(u_key):
-                        ulcer_factor_dict[b_key] = u_pxls[0]/b_pxls[0]
-                        print( 'u pixels' ,u_pxls[0])
-                        print( ' b  pixels' , b_pxls[0])
-                        print( 'relation ', u_pxls[0]/b_pxls[0])
-                        break
-                        
+    body_values.append(b_pxls[0])
+
+for u_key, u_pxls in ulcer_json.items():
+        ulcer_values.append(u_pxls[0])
+        image_ids.append(u_key)
+
+for i in range(len(ulcer_values)):
+        ulcer_factor_dict[image_ids[i]] = ulcer_values[i]/body_values[i]
+
+print(body_json)
+print("__________________________")
+print(ulcer_json)
 
 with open('ulcer_factor.json', 'w') as f:
     json.dump(ulcer_factor_dict, f)
+
+print(ulcer_factor_dict)
